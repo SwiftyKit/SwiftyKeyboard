7a571b8d1b804eebdfebe5e52ca7d898449d1460
Roman Kavisnkyi <90592056+Kavinskyi@users.noreply.github.com>
2022-03-22 20:19:44 +0700

    Update SwiftyKeyboard.swift

--- Sources/SwiftyKeyboard.swift
+++ Sources/SwiftyKeyboard.swift
@@ -16,7 +16,7 @@ import UIKit
 @objcMembers public class SwiftyKeyboard: UIInputView, UIInputViewAudioFeedback {
 
     // MARK: - UIInputViewAudioFeedback
-    private var enableInputClicksWhenVisible: Bool = true
+    public var enableInputClicksWhenVisible: Bool = true
 
     // MARK: - Constants
     private let keyboardRows                = 4
